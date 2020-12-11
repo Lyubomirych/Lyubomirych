434cdf64f1cac1b91c1787a56938f6e2d96b4142
Lyubomirych <lyubchik2001@mail.ru>
2020-12-11 19:18:09 +0300

    26 и 27 C++

---
+++ Practice/26/C++/26.cpp
@@ -0,0 +1,151 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+template<typename T>
+using new_vector = vector<T>;
+template<typename T>
+using new_matrix = vector<vector<T>>;
+
+
+
+
+
+template<typename T>
+bool sort(new_vector<T> v, bool direction = true) {//Выражение в зависимости от значения умолчания
+
+    int vect_len = v.size();
+    if (direction) {
+        for (int i = 1; i < vect_len; i++) {
+
+            if (v[i - 1] > v[i]) {
+                return false;
+            }
+
+        }
+    }
+    else {
+        for (int i = 1; i < vect_len; i++) {
+
+            if (v[i - 1] < v[i]) {
+                return false;
+            }
+
+        }
+    }
+    return true;
+
+}
+
+
+template<typename T>
+new_vector<T> BozoSort(new_vector<T> v, bool direction = true) { //Вектор
+    int vect_len = v.size();
+    while (!sort(v, direction))
+    {
+        int index1 = rand() % vect_len;
+        int index2 = rand() % vect_len;
+        swap(v[index1], v[index2]);
+
+    }
+    return v;
+}
+
+
+template<typename T>
+new_vector<T> BozoSort(new_matrix<T> v_dvumer, bool direction = true) { //Двумерный массив
+    new_vector<T> v_dvumer2;
+    for (new_vector<T> i : v_dvumer) {
+        for (T j : i) {
+            v_dvumer2.push_back(j);
+        }
+    }
+    int vect_len = v_dvumer2.size();
+    while (!sort(v_dvumer2, direction))
+    {
+        int index1 = rand() % vect_len;
+        int index2 = rand() % vect_len;
+        swap(v_dvumer2[index1], v_dvumer2[index2]);
+
+    }
+    return v_dvumer2;
+}
+
+
+template<typename T>
+new_vector<T> BozoSort(T x, T y, T z, bool direction = true) { // 3 элемента
+    new_vector<T> v3;
+    v3 = { x, y, z };
+    int vect_len = v3.size();
+    while (!sort(v3, direction)) {
+        int index1 = rand() % vect_len;
+        int index2 = rand() % vect_len;
+        swap(v3[index1], v3[index2]);
+    }
+
+    return v3;
+}
+
+
+template<typename T>
+pair<new_vector<T>, new_matrix<T>> get_user_info()
+{
+    int a; T element;
+    cout << "Введите количество элементов от 4 до 100, из которых можно извлечь целый квадратный корень:\n";
+    cin >> a;
+    
+    new_vector<T> v;            //вектор
+    new_matrix<T> vector_matrix;//матрица
+    new_vector<T> formatrix;    //вектор для заполнения двумерного вектора
+
+    cout << "Введите элементы массива через пробел: ";
+    for (int i = 1; i <= a; i++)
+    {
+        cin >> element;
+        formatrix.push_back(element);
+        v.push_back(element);
+        if (i % int(sqrt(a)) == 0)
+        {
+            vector_matrix.push_back(formatrix);
+            formatrix.clear();
+        }
+    }
+    return { v, vector_matrix };
+}
+
+template<typename T>
+void print_vector(new_vector<T> arr) { // Функция для вывода векторов
+    for (T i : arr) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    setlocale(0, "");
+
+    auto doubl = get_user_info<double>();
+    auto str = get_user_info<string>();
+
+    new_vector<double> vector_double = doubl.first;
+    new_matrix<double> matrix_double = doubl.second;
+    new_vector<string> vector_string = str.first;
+    new_matrix<string> matrix_string = str.second;
+
+    print_vector(BozoSort(vector_double, true));
+    print_vector(BozoSort(vector_double, false));
+    print_vector(BozoSort(matrix_double, true));
+    print_vector(BozoSort(matrix_double, false));
+    print_vector(BozoSort(vector_double[0], vector_double[1], vector_double[2], true));
+    print_vector(BozoSort(vector_double[0], vector_double[1], vector_double[2], false));
+
+    print_vector(BozoSort(vector_string, true));
+    print_vector(BozoSort(vector_string, false));
+    print_vector(BozoSort(matrix_string, true));
+    print_vector(BozoSort(matrix_string, false));
+    print_vector(BozoSort(vector_string[0], vector_string[1], vector_string[2], true));
+    print_vector(BozoSort(vector_string[0], vector_string[1], vector_string[2], false));
+}
+
---
+++ Practice/27/C++/27.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iterator>
+#include <set> //подключаю библиотеку множества
+
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int n;
+    cout << "Введите количество сигалов n, 1<=n<=100000: ";
+    cin >> n;
+    if (1 > n || n > 100000) { cout << "Данные введены неверно!"; }
+    else {
+        set<int> a;
+        cout << "Введите целые числа a, -1000000<=a<=1000000: ";
+        for (int i = 0; i < n; ++i) {
+            int t;
+            cin >> t;
+            if (-1000000 > t || t > 1000000) { cout << "Данные не соответствуют условию!"; break; }
+            a.insert(t);
+            int j = 0;
+            set<int> b;
+            for (auto it = a.cbegin(); it != a.cend() && j < 5; ++it, ++j)
+                b.insert(*it);
+            copy(b.crbegin(), b.crend(), ostream_iterator<int>(cout, " "));
+            cout << '\n';
+        }
+    }
+}
