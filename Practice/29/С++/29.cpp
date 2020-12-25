790060252beb58ea6c6e00ef20fdf61871f99c76
Lyubomirych <lyubchik2001@mail.ru>
2020-12-25 14:33:44 +0300

    Обновление прототипа 29

--- Practice/29/С++/29.cpp
+++ Practice/29/С++/29.cpp
@@ -6,39 +6,59 @@
 
 using namespace std;
 
-string arr[] = { "math", "phys", "hist", "prog" };
+string arr[] = { "Math", "Phys", "Hist", "Progg" };
 
 struct Student
 {
     string name;
     int group;
+    // Math | Phys | Hist | Prog
     map<string, int> exams;
     map<string, int>::iterator examsInit;
     int n;
-
-    void vvod()
+    void init()
     {
-        cout << "Имя: "; cin >> name;
-        cout << "Группа: "; cin >> group;
+        cout << "Name of students: ";
+        cin >> name;
+        cout << "Group: ";
+        cin >> group;
         for (int i = 0; i < 4; i++)
         {
-            cout << arr[i] << " категория: ";
-            cin >> exams[arr[i]];
+           cout << arr[i] << " score: ";
+           cin >> exams[arr[i]];
         }
         cout << '\n';
     }
+
 };
 
-
+std::ostream& operator<<(std::ostream& out, Student st)
+{
+      //Ввод и вывод на английском, потому что корявый вывод русского, даже с локалью
+    out << "| Name       | Group |";
+    for (int i = 0; i < 4; i++)
+    {
+        out << std::setw(6) << arr[i] << "|";
+    }
+    out << '\n';
+    out << '|' << std::setw(12) << st.name << '|' << std::setw(7) << st.group << '|';
+    for (st.examsInit = st.exams.begin(); st.examsInit != st.exams.end(); st.examsInit++)
+    {
+        out << std::setw(6) << st.examsInit->second << '|';
+    }
+    out << '\n';
+    return out;
+}
 
 
 int main()
 {
-    setlocale(0, "");
     int n;
-    cout << "сколько студентов?\n";
+    cout << "How mant studensts?\n";
     cin >> n;
+
     vector<Student> students(n);
     for (auto& student : students)
-        student.vvod();
+        student.init();
+    cout << students[0];
 }
